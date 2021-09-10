56e2a4a87006cfcaf9e7c1fe73fccf448e2ef34a
Fel <ghuyfelk@live.fr>
2021-09-10 22:52:55 +0200

    Overrode AzureException toString method for better debugging

--- lib/src/common/azure_exception.dart
+++ lib/src/common/azure_exception.dart
@@ -3,4 +3,9 @@ import 'package:flutter_azure_tts/src/common/base_response.dart';
 class AzureException implements Exception {
   AzureException({required this.response});
   final BaseResponse response;
+
+  @override
+  String toString() {
+    return "[AzureException] ${response.code}: ${response.reason}";
+  }
 }
